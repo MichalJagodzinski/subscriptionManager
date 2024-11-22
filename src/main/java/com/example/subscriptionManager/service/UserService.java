408504e3ebfe3d37401ffb317594d82ce268ca16
unknown <mjagodzinski53@hmail.com>
2024-11-22 20:20:46 +0100

    #3

--- src/main/java/com/example/subscriptionManager/service/UserService.java
+++ src/main/java/com/example/subscriptionManager/service/UserService.java
@@ -29,4 +29,9 @@ public class UserService {
 
         return userRepository.save(user);
     }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
 }
